4a40517d357417ca234270e49db5cd54f82e736d
marianacarbonell <marianacarbonellm@gmail.com>
2019-03-20 17:50:49 -0300

    probando rama nueva

--- src/Componentes/Navbar/NavBar.js
+++ src/Componentes/Navbar/NavBar.js
@@ -7,26 +7,26 @@ class NavbarPag extends Component {
 
     render() {
         return (
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
+<nav className="navbar navbar-expand-lg">
+<i class="material-icons">Home</i>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
   </button>
-  <div class="collapse navbar-collapse" id="navbarNavDropdown">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="../../App.js">Home <span class="sr-only">(current)</span></a>
+  <div className="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul className="navbar-nav">
+      <li className="nav-item active">
+        <a className="nav-link" href="../../App.js">Home <span class="sr-only">(current)</span></a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="../Catalogo">Emprendedores</a>
+      <li className="nav-item">
+        <a className="nav-link" href="../Catalogo">Emprendedores</a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="../Formulario/formulario.js">Editar Perfil</a>
+      <li className="nav-item">
+        <a className="nav-link" href="../Formulario/formulario.js">Editar Perfil</a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="../../App.js">Cerrar Sesión</a>
+      <li className="nav-item">
+        <a className="nav-link" href="../../App.js">Cerrar Sesión</a>
       </li>
-      <li class="nav-item dropdown">
+      <li className="nav-item dropdown">
       </li>
     </ul>
   </div>
