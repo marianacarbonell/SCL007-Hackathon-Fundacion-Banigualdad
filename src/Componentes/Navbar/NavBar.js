4718db83694d9cdc0bba9ce019aeaf961f229faa
marianacarbonell <marianacarbonellm@gmail.com>
2019-03-20 18:26:11 -0300

    buscando imagen volantin

--- src/Componentes/Navbar/NavBar.js
+++ src/Componentes/Navbar/NavBar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './Navbar.css';
 
 class NavbarPag extends Component {
     constructor(props) {
@@ -7,26 +8,15 @@ class NavbarPag extends Component {
 
     render() {
         return (
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
+<nav className="navbar navbar-expand-lg navbar-light">
+<img src="../Imagenes/VOLANTIN.png"/>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
   </button>
-  <div class="collapse navbar-collapse" id="navbarNavDropdown">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="../../App.js">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="../Catalogo">Emprendedores</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="../Formulario/formulario.js">Editar Perfil</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="../../App.js">Cerrar Sesión</a>
-      </li>
-      <li class="nav-item dropdown">
+  <div className="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul className="navbar-nav">
+      <li className="nav-item active">
+        <a className="nav-link" href="../../App.js">Home <span className="sr-only">(current)</span></a>
       </li>
     </ul>
   </div>
